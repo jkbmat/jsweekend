3ecb27c5ce9cf507b288c8bdf3d7ba81783cd7c7
Jakub <jkb.matuska@gmail.com>
2018-03-05 16:24:55 +0100

    Add github link to the header

--- src/components/general/Header/Header.tsx
+++ src/components/general/Header/Header.tsx
@@ -1,11 +1,16 @@
 import * as React from 'react'
+
+import githubLogo from 'img/github-logo.png'
 import './Header.css'
 
 export default class Header extends React.Component {
 	render () {
 		return (
 			<div className='Header'>
-				JS Weekend: Find your flight!
+				<div className='Header__title'>JS Weekend: Find your flight!</div>
+				<a className='Header__logo' href='https://github.com/jkbmat/jsweekend/' target='_blank'>
+					<img src={githubLogo} width={32} height={32} />
+				</a>
 			</div>
 		)
 	}
