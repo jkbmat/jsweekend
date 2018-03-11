11f74a7a2205e641cb5ecf153bf9c77168a9c576
Jakub Matuska <jkb.matuska@gmail.com>
2018-03-11 22:09:14 +0100

    Preload the github avatar in Header

--- src/components/general/Header/Header.tsx
+++ src/components/general/Header/Header.tsx
@@ -3,13 +3,17 @@ import * as React from 'react'
 import './Header.css'
 
 export default class Header extends React.Component {
+	componentWillMount () {
+		const avatarPreloader = new Image()
+		avatarPreloader.src = 'https://avatars.githubusercontent.com/jkbmat?size=32'
+	}
+
 	render () {
 		return (
 			<div className='Header'>
 				<div className='Header__title'>JS Weekend: Find your flight!</div>
 				<a className='Header__logo' href='https://github.com/jkbmat/jsweekend/' target='_blank'>
 					<div className='Header__logo-image'/>
-					{/*<img src='https://avatars.githubusercontent.com/jkbmat?size=32' width={32} height={32} />*/}
 				</a>
 			</div>
 		)
